a163d2b31cd24e3807e78a41669a05d2e4b2795c
jakeduan <duanhong169@gmail.com>
2025-01-08 16:41:59 +0800

    remove unused code

--- android/samples/sample-aixinxin-test/src/main/java/com/google/android/filament/aixinxin/MainActivity.kt
+++ android/samples/sample-aixinxin-test/src/main/java/com/google/android/filament/aixinxin/MainActivity.kt
@@ -49,25 +49,20 @@ class MainActivity : Activity() {
     companion object {
         // Load the library for the utility layer, which in turn loads gltfio and the Filament core.
         init { Utils.init() }
-        private const val TAG = "gltf-viewer"
+        private const val TAG = "aixinxin"
     }
 
     private lateinit var surfaceView: SurfaceView
     private lateinit var choreographer: Choreographer
     private val frameScheduler = FrameCallback()
     private lateinit var modelViewer: ModelViewer
-    private lateinit var titlebarHint: TextView
     private val doubleTapListener = DoubleTapListener()
     private val singleTapListener = SingleTapListener()
     private lateinit var doubleTapDetector: GestureDetector
     private lateinit var singleTapDetector: GestureDetector
-    private var remoteServer: RemoteServer? = null
     private var statusToast: Toast? = null
     private var statusText: String? = null
-    private var latestDownload: String? = null
     private val automation = AutomationEngine()
-    private var loadStartTime = 0L
-    private var loadStartFence: Fence? = null
     private val viewerContent = AutomationEngine.ViewerContent()
 
     @SuppressLint("ClickableViewAccessibility")
@@ -76,7 +71,6 @@ class MainActivity : Activity() {
         setContentView(R.layout.simple_layout)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-        titlebarHint = findViewById(R.id.user_hint)
         surfaceView = findViewById(R.id.main_sv)
         choreographer = Choreographer.getInstance()
 
@@ -100,9 +94,9 @@ class MainActivity : Activity() {
         createDefaultRenderables()
         createIndirectLight()
 
-        setStatusText("To load a new model, go to the above URL on your host machine.")
-
         val view = modelViewer.view
+        modelViewer.cameraNear = 0.1f
+        modelViewer.cameraFar = 10000.0f
 
         /*
          * Note: The settings below are overriden when connecting to the remote UI.
@@ -136,21 +130,34 @@ class MainActivity : Activity() {
         view.bloomOptions = view.bloomOptions.apply {
             enabled = true
         }
-
-        remoteServer = RemoteServer(8082)
     }
 
     private fun createDefaultRenderables() {
-        val buffer = assets.open("models/bodyLow/AiXinXin_Rig_bodyCloth_low_v01.gltf").use { input ->
-//        val buffer = assets.open("models/headLow/AiXinXin_Rig_head_low_v01.gltf").use { input ->
+//        val assetsPathBase = "models/allLowidle"
+        val assetsPathBase = "models/allLowStand01RB001"
+//        val assetsPathBase = "models/merged"
+//        val assetsPath = "${assetsPathBase}/AiXinXin_Rig_all_low.gltf"
+        val assetsPath = "${assetsPathBase}/AiXinXin_Rig_all_low_Stand01RB001.gltf"
+//        val assetsPath = "${assetsPathBase}/merged.gltf"
+        val buffer = assets.open(assetsPath).use { input ->
             val bytes = ByteArray(input.available())
             input.read(bytes)
             ByteBuffer.wrap(bytes)
         }
 
-        modelViewer.loadModelGltfAsync(buffer) { uri -> readCompressedAsset("models/bodyLow/$uri") }
-//        modelViewer.loadModelGltfAsync(buffer) { uri -> readCompressedAsset("models/headLow/$uri") }
+        modelViewer.loadModelGltfAsync(buffer) { uri -> readCompressedAsset("${assetsPathBase}/$uri") }
         updateRootTransform()
+
+        if (assetsPathBase.contains("merged")){
+            val tm = modelViewer.engine.transformManager
+            var center = Float3(-0.0022023767f, 0.901152f, 0.035443634f )
+            val halfExtent = Float3(0.23230186f, 0.78089356f, 0.17928179f)
+            val maxExtent = 2.0f * max(halfExtent)
+            val scaleFactor = 2.0f / maxExtent
+            center -= Float3(0.0f, -0f, -2f) / scaleFactor
+            val transform = scale(Float3(scaleFactor)) * translation(-center)
+            tm.setTransform(tm.getInstance(modelViewer.asset!!.root), transpose(transform).toFloatArray())
+        }
     }
 
     private fun createIndirectLight() {
@@ -187,146 +194,7 @@ class MainActivity : Activity() {
                 statusText = text
                 statusToast = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
                 statusToast!!.show()
-
             }
-        }
-    }
-
-    private suspend fun loadGlb(message: RemoteServer.ReceivedMessage) {
-        withContext(Dispatchers.Main) {
-            modelViewer.destroyModel()
-            modelViewer.loadModelGlb(message.buffer)
-            updateRootTransform()
-            loadStartTime = System.nanoTime()
-            loadStartFence = modelViewer.engine.createFence()
-        }
-    }
-
-    private suspend fun loadHdr(message: RemoteServer.ReceivedMessage) {
-        withContext(Dispatchers.Main) {
-            val engine = modelViewer.engine
-            val equirect = HDRLoader.createTexture(engine, message.buffer)
-            if (equirect == null) {
-                setStatusText("Could not decode HDR file.")
-            } else {
-                setStatusText("Successfully decoded HDR file.")
-
-                val context = IBLPrefilterContext(engine)
-                val equirectToCubemap = IBLPrefilterContext.EquirectangularToCubemap(context)
-                val skyboxTexture = equirectToCubemap.run(equirect)!!
-                engine.destroyTexture(equirect)
-
-                val specularFilter = IBLPrefilterContext.SpecularFilter(context)
-                val reflections = specularFilter.run(skyboxTexture)
-
-                val ibl = IndirectLight.Builder()
-                         .reflections(reflections)
-                         .intensity(30000.0f)
-                         .build(engine)
-
-                val sky = Skybox.Builder().environment(skyboxTexture).build(engine)
-
-                specularFilter.destroy()
-                equirectToCubemap.destroy()
-                context.destroy()
-
-                // destroy the previous IBl
-                engine.destroyIndirectLight(modelViewer.scene.indirectLight!!)
-                engine.destroySkybox(modelViewer.scene.skybox!!)
-
-                modelViewer.scene.skybox = sky
-                modelViewer.scene.indirectLight = ibl
-                viewerContent.indirectLight = ibl
-
-            }
-        }
-    }
-
-    private suspend fun loadZip(message: RemoteServer.ReceivedMessage) {
-        // To alleviate memory pressure, remove the old model before deflating the zip.
-        withContext(Dispatchers.Main) {
-            modelViewer.destroyModel()
-        }
-
-        // Large zip files should first be written to a file to prevent OOM.
-        // It is also crucial that we null out the message "buffer" field.
-        val (zipStream, zipFile) = withContext(Dispatchers.IO) {
-            val file = File.createTempFile("incoming", "zip", cacheDir)
-            val raf = RandomAccessFile(file, "rw")
-            raf.channel.write(message.buffer)
-            message.buffer = null
-            raf.seek(0)
-            Pair(FileInputStream(file), file)
-        }
-
-        // Deflate each resource using the IO dispatcher, one by one.
-        var gltfPath: String? = null
-        var outOfMemory: String? = null
-        val pathToBufferMapping = withContext(Dispatchers.IO) {
-            val deflater = ZipInputStream(zipStream)
-            val mapping = HashMap<String, Buffer>()
-            while (true) {
-                val entry = deflater.nextEntry ?: break
-                if (entry.isDirectory) continue
-
-                // This isn't strictly required, but as an optimization
-                // we ignore common junk that often pollutes ZIP files.
-                if (entry.name.startsWith("__MACOSX")) continue
-                if (entry.name.startsWith(".DS_Store")) continue
-
-                val uri = entry.name
-                val byteArray: ByteArray? = try {
-                    deflater.readBytes()
-                }
-                catch (e: OutOfMemoryError) {
-                    outOfMemory = uri
-                    break
-                }
-                Log.i(TAG, "Deflated ${byteArray!!.size} bytes from $uri")
-                val buffer = ByteBuffer.wrap(byteArray)
-                mapping[uri] = buffer
-                if (uri.endsWith(".gltf") || uri.endsWith(".glb")) {
-                    gltfPath = uri
-                }
-            }
-            mapping
-        }
-
-        zipFile.delete()
-
-        if (gltfPath == null) {
-            setStatusText("Could not find .gltf or .glb in the zip.")
-            return
-        }
-
-        if (outOfMemory != null) {
-            setStatusText("Out of memory while deflating $outOfMemory")
-            return
-        }
-
-        val gltfBuffer = pathToBufferMapping[gltfPath]!!
-
-        // In a zip file, the gltf file might be in the same folder as resources, or in a different
-        // folder. It is crucial to test against both of these cases. In any case, the resource
-        // paths are all specified relative to the location of the gltf file.
-        var prefix = URI(gltfPath!!).resolve(".")
-
-        withContext(Dispatchers.Main) {
-            if (gltfPath!!.endsWith(".glb")) {
-                modelViewer.loadModelGlb(gltfBuffer)
-            } else {
-                modelViewer.loadModelGltf(gltfBuffer) { uri ->
-                    val path = prefix.resolve(uri).toString()
-                    if (!pathToBufferMapping.contains(path)) {
-                        Log.e(TAG, "Could not find '$uri' in zip using prefix '$prefix' and base path '${gltfPath!!}'")
-                        setStatusText("Zip is missing $path")
-                    }
-                    pathToBufferMapping[path]
-                }
-            }
-            updateRootTransform()
-            loadStartTime = System.nanoTime()
-            loadStartFence = modelViewer.engine.createFence()
         }
     }
 
@@ -343,36 +211,13 @@ class MainActivity : Activity() {
     override fun onDestroy() {
         super.onDestroy()
         choreographer.removeFrameCallback(frameScheduler)
-        remoteServer?.close()
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
+        @Suppress("DEPRECATION")
         super.onBackPressed()
         finish()
-    }
-
-    fun loadModelData(message: RemoteServer.ReceivedMessage) {
-        Log.i(TAG, "Downloaded model ${message.label} (${message.buffer.capacity()} bytes)")
-        clearStatusText()
-        titlebarHint.text = message.label
-        CoroutineScope(Dispatchers.IO).launch {
-            when {
-                message.label.endsWith(".zip") -> loadZip(message)
-                message.label.endsWith(".hdr") -> loadHdr(message)
-                else -> loadGlb(message)
-            }
-        }
-    }
-
-    fun loadSettings(message: RemoteServer.ReceivedMessage) {
-        val json = StandardCharsets.UTF_8.decode(message.buffer).toString()
-        viewerContent.assetLights = modelViewer.asset?.lightEntities
-        automation.applySettings(modelViewer.engine, json, viewerContent)
-        modelViewer.view.colorGrading = automation.getColorGrading(modelViewer.engine)
-        modelViewer.cameraFocalLength = automation.viewerOptions.cameraFocalLength
-        modelViewer.cameraNear = automation.viewerOptions.cameraNear
-        modelViewer.cameraFar = automation.viewerOptions.cameraFar
-        updateRootTransform()
     }
 
     private fun updateRootTransform() {
@@ -388,46 +233,6 @@ class MainActivity : Activity() {
         override fun doFrame(frameTimeNanos: Long) {
             choreographer.postFrameCallback(this)
 
-            loadStartFence?.let {
-                if (it.wait(Fence.Mode.FLUSH, 0) == Fence.FenceStatus.CONDITION_SATISFIED) {
-                    val end = System.nanoTime()
-                    val total = (end - loadStartTime) / 1_000_000
-                    Log.i(TAG, "The Filament backend took $total ms to load the model geometry.")
-                    modelViewer.engine.destroyFence(it)
-                    loadStartFence = null
-
-                    val materials = mutableSetOf<Material>()
-                    val rcm = modelViewer.engine.renderableManager
-                    modelViewer.scene.forEach {
-                        val entity = it
-                        if (rcm.hasComponent(entity)) {
-                            val ri = rcm.getInstance(entity)
-                            val c = rcm.getPrimitiveCount(ri)
-                            for (i in 0 until c) {
-                                val mi = rcm.getMaterialInstanceAt(ri, i)
-                                val ma = mi.material
-                                materials.add(ma)
-                            }
-                        }
-                    }
-                    materials.forEach {
-                        it.compile(
-                            Material.CompilerPriorityQueue.HIGH,
-                            Material.UserVariantFilterBit.DIRECTIONAL_LIGHTING or
-                            Material.UserVariantFilterBit.DYNAMIC_LIGHTING or
-                            Material.UserVariantFilterBit.SHADOW_RECEIVER,
-                            null, null)
-                        it.compile(
-                            Material.CompilerPriorityQueue.LOW,
-                            Material.UserVariantFilterBit.FOG or
-                            Material.UserVariantFilterBit.SKINNING or
-                            Material.UserVariantFilterBit.SSR or
-                            Material.UserVariantFilterBit.VSM,
-                            null, null)
-                    }
-                }
-            }
-
             modelViewer.animator?.apply {
                 if (animationCount > 0) {
                     val elapsedTimeSeconds = (frameTimeNanos - startTime).toDouble() / 1_000_000_000
@@ -437,27 +242,6 @@ class MainActivity : Activity() {
             }
 
             modelViewer.render(frameTimeNanos)
-
-            // Check if a new download is in progress. If so, let the user know with toast.
-            val currentDownload = remoteServer?.peekIncomingLabel()
-            if (RemoteServer.isBinary(currentDownload) && currentDownload != latestDownload) {
-                latestDownload = currentDownload
-                Log.i(TAG, "Downloading $currentDownload")
-                setStatusText("Downloading $currentDownload")
-            }
-
-            // Check if a new message has been fully received from the client.
-            val message = remoteServer?.acquireReceivedMessage()
-            if (message != null) {
-                if (message.label == latestDownload) {
-                    latestDownload = null
-                }
-                if (RemoteServer.isJson(message.label)) {
-                    loadSettings(message)
-                } else {
-                    loadModelData(message)
-                }
-            }
         }
     }
 
